0384d5fbd094fc5e50417c0522d8d9d99c94fee8
suryasiriki4 <cs18b035@iittp.ac.in>
2020-12-24 18:31:12 +0530

    first commit

---
+++ src/study_material/StudyMaterial.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../NavBar";
+
+
+function StudyMaterial() {
+    return(
+        <div>
+            <NavBar />
+            <h1>StudyMaterial</h1>
+        </div>
+    );
+}
+
+export default StudyMaterial;
